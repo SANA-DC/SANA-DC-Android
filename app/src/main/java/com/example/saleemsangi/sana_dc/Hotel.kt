bed2d001901e16a7692601d6060f05b0b9a4a778
saleemsangi <greatsangi@yahoo.com>
2018-02-18 13:09:21 -0500

    Loading hotel

--- app/src/main/java/com/example/saleemsangi/sana_dc/Hotel.kt
+++ app/src/main/java/com/example/saleemsangi/sana_dc/Hotel.kt
@@ -2,9 +2,15 @@ package com.example.saleemsangi.sana_dc
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.fragment_hotel.*
+import kotlinx.android.synthetic.main.fragment_welcome.*
 
 /**
  * Created by saleemsangi on 2/17/18.
@@ -16,5 +22,21 @@ class Hotel: Fragment(){
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         return inflater?.inflate(R.layout.fragment_hotel, null)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = object : WebViewClient(){
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                view?.loadUrl(request?.url.toString())
+                return true
+            }
+        }
+
+        webView.loadUrl("https://aws.passkey.com/e/49513241")
+
     }
 }
