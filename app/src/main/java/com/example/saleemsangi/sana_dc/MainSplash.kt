b634514aee959dd8315df64263b65c9ff9020e91
saleemsangi <greatsangi@yahoo.com>
2018-05-08 22:34:03 -0400

    added splash screen

---
+++ app/src/main/java/com/example/saleemsangi/sana_dc/MainSplash.kt
@@ -0,0 +1,35 @@
+package com.example.saleemsangi.sana_dc
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainSplash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+
+
+
+
+
+        val background = object : Thread(){
+            override fun run() {
+                super.run()
+                try {
+                    Thread.sleep(1000)
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+
+                } catch (e: Exception){
+
+                }
+            }
+        }
+        background.start()
+    }
+
+}
