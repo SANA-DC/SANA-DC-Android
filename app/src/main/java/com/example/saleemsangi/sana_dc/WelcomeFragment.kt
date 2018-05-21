0862461df7427d4fab459785d1b38661276ca2e6
saleemsangi <greatsangi@yahoo.com>
2018-05-20 23:54:38 -0400

    Added animation for arrow

--- app/src/main/java/com/example/saleemsangi/sana_dc/WelcomeFragment.kt
+++ app/src/main/java/com/example/saleemsangi/sana_dc/WelcomeFragment.kt
@@ -6,7 +6,12 @@ import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.LinearInterpolator
 import kotlinx.android.synthetic.main.fragment_welcome.*
+
+
 
 
 
@@ -27,6 +32,36 @@ class WelcomeFragment : Fragment(){
         activity.title = "Welcome"
 
         return inflater?.inflate(R.layout.fragment_welcome, null)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+
+        val sharedPerfrence = context.getSharedPreferences("arrow",0 )
+
+        val showArrow = sharedPerfrence.getBoolean("arrowInimation", false)
+
+        if (!showArrow){
+            val blinkanimation = AlphaAnimation(1f, 0f) // Change alpha from fully visible to invisible
+            blinkanimation.duration = 500 // duration
+            blinkanimation.interpolator = LinearInterpolator() // do not alter animation rate
+            blinkanimation.repeatCount = Animation.INFINITE // Repeat animation infinitely
+            blinkanimation.repeatMode = Animation.REVERSE
+            arrowImage.animation = blinkanimation
+
+            val editor  = sharedPerfrence.edit()
+            editor.putBoolean("arrowInimation", true)
+            editor.apply()
+        } else {
+            arrowImage.visibility = View.INVISIBLE
+        }
+
+
+
+
+
+
     }
 
 }
